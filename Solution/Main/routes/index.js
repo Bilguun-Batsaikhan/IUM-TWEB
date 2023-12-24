ad5bbdffe81495c5e786e87032b86b6f82018225
Emanuele Machetti <emanuele.machetti@edu.unito.it>
2023-12-24 17:35:39 +0100

    draft implementation of the chat

--- Solution/Main/routes/index.js
+++ Solution/Main/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
+const path = require('path');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+router.get('/chat-static', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/chat.html'));
+});
+router.get('/chat', function(req, res, next) {
+  res.render('chat', { title: 'Chat' });
 });
 
 module.exports = router;
---
+++ Solution/Main/socket.io/socket-io.js
@@ -0,0 +1,29 @@
+
+exports.init = function(io) {
+
+    // the chat namespace
+    const chat= io
+        .of('/chat')
+        .on('connection', function (socket) {
+            try {
+                /**
+                 * it creates or joins a room
+                 */
+                socket.on('create or join', function (room, userId) {
+                    socket.join(room);
+                    chat.to(room).emit('joined', room, userId);
+                });
+
+                socket.on('chat', function (room, userId, chatText) {
+                    chat.to(room).emit('chat', room, userId, chatText);
+                });
+
+                socket.on('disconnect', function(){
+                    console.log('someone disconnected');
+                });
+            } catch (e) {
+            }
+        });
+
+
+}
