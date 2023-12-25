6516d4cc0277e304d1c7d97527ced7b546d0962c
Emanuele Machetti <emanuele.machetti@edu.unito.it>
2023-12-25 18:45:31 +0100

    Implementation of the chat with choice of club room

--- Solution/Main/public/javascripts/chat.js
+++ Solution/Main/public/javascripts/chat.js
@@ -1,3 +1,115 @@
+let name = null;
+let roomNo = null;
+let chat= io.connect('/chat');
+
+
+
+/**
+ * called by <body onload>
+ * it initialises the interface and the expected socket messages
+ * plus the associated actions
+ */
 function init() {
-   console.log("Test");
+   // it sets up the interface so that userId and room are selected
+   document.getElementById('initial_form').style.display = 'block';
+   document.getElementById('chat_interface').style.display = 'none';
+
+   // it updates the select with the list of rooms
+   var select = document.getElementById('roomNo');
+   select.innerHTML = '';
+
+   clubs.forEach(function (club) {
+      var option = document.createElement('option');
+      option.value = club.name;
+      option.textContent = club.name;
+      select.appendChild(option);
+   });
+
+   //console.log(clubs);
+   //console.log(nickname);
+   initChatSocket();
 }
+
+
+/**
+ * it initialises the socket for /chat
+ */
+function initChatSocket() {
+   // called when someone joins the room. If it is someone else it notifies the joining of the room
+   chat.on('joined', function (room, userId) {
+      if (userId === name) {
+         // it enters the chat
+         hideLoginInterface(room, userId);
+      } else {
+         // notifies that someone has joined the room
+         writeOnChatHistory('<b>' + userId + '</b>' + ' joined room ' + room);
+      }
+   });
+   // called when a message is received
+   chat.on('chat', function (room, userId, chatText) {
+      let who = userId
+      if (userId === name) who = 'Me';
+      writeOnChatHistory('<b>' + who + ':</b> ' + chatText);
+   });
+
+}
+
+
+
+/**
+ * called when the Send button is pressed. It gets the text to send from the interface
+ * and sends the message via  socket
+ */
+function sendChatText() {
+   let chatText = document.getElementById('chat_input').value;
+   chat.emit('chat', roomNo, name, chatText);
+}
+
+/**
+ * used to connect to a room. It gets the user name and room number from the
+ * interface
+ * It connects both chat and news at the same time
+ */
+function connectToRoom() {
+   roomNo = document.getElementById('roomNo').value;
+   name = nickname;
+   if (!name) name = 'Unknown-' + Math.random();
+   chat.emit('create or join', roomNo, name);
+}
+
+/**
+ * it appends the given html text to the history div
+ * @param text: teh text to append
+ */
+function writeOnChatHistory(text) {
+   let history = document.getElementById('chat_history');
+   let paragraph = document.createElement('p');
+   paragraph.innerHTML = text;
+   history.appendChild(paragraph);
+   document.getElementById('chat_input').value = '';
+}
+
+/**
+ * it appends the given html text to the history div
+ * @param text: teh text to append
+ */
+function writeOnNewsHistory(text) {
+   let history = document.getElementById('news_history');
+   let paragraph = document.createElement('p');
+   paragraph.innerHTML = text;
+   history.appendChild(paragraph);
+   document.getElementById('news_input').value = '';
+}
+
+/**
+ * it hides the initial form and shows the chat
+ * @param room the selected room
+ * @param userId the user name
+ */
+function hideLoginInterface(room, userId) {
+   document.getElementById('initial_form').style.display = 'none';
+   document.getElementById('chat_interface').style.display = 'block';
+   document.getElementById('who_you_are').innerHTML= userId;
+   document.getElementById('in_room').innerHTML= ' '+room;
+}
+
--- Solution/Main/routes/index.js
+++ Solution/Main/routes/index.js
@@ -11,8 +11,27 @@ router.get('/', function(req, res, next) {
 router.get('/chat-static', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../public/chat.html'));
 });
-router.get('/chat', function(req, res, next) {
-  res.render('chat', { title: 'Chat' });
+
+/* The nickname should be retrieved from the login page.
+   The list of clubs should be retrieved from the database.
+*/
+var nickname = 'Admin';
+const clubs = [
+  { name: "Juventus",
+  },
+  {
+    name: "Inter",
+  },
+  {
+    name: "Milan",
+  },
+];
+router.get('/chat', (req, res) => {
+  if (nickname !== null && typeof nickname !== 'undefined') {
+    res.render('chat', { title: 'Chat', nickname, clubs });
+  } else {
+    res.render('error', { error: 'login required', message: 'To use the chat, it is necessary to be logged in.' });
+  }
 });
 
 module.exports = router;
