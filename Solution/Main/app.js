2e3ee847c5e05c2c00e3164a1c48e7088d753008
Emanuele Machetti <emanuele.machetti@edu.unito.it>
2023-12-25 18:56:34 +0100

    fixing missing file

--- Solution/Main/app.js
+++ Solution/Main/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
 var app = express();
 
 // view engine setup
@@ -22,6 +23,7 @@ app.use(express.static('public'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
