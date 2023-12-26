7cc94f0acea1a174359eb3d87d7a5ad99bddcfb2
Alex Agliuzza <alex.agliuzza@edu.unito.it>
2023-12-26 17:42:45 +0100

    waitingPage & require mongoose

---
+++ Solution/Main/databases/database.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+//The URL which will be queried. Run "mongod.exe" for this to connect
+//var url = 'mongodb://localhost:27017/test';
+const mongoDB = 'mongodb://localhost:27017/characters';
+mongoose.Promise = global.Promise;
+connection = mongoose.connect(mongoDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    checkServerIdentity: false,
+    family: 4
+})
+    .then(() => {
+        console.log('connection to mongodb worked!');
+    })
+    .catch((error) => {
+        console.log('connection to mongodb did not work! ' + JSON.stringify(error));
+    });
