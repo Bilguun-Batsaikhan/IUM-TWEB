5f58a9729d41bd470fa9bf6ec5dde910387789ce
Emanuele Machetti <emanuele.machetti@edu.unito.it>
2024-01-06 17:52:26 +0100

    route /valutation/club moved to the new file routes/valutation.js, minor graphical changes to the club_valutation page

--- Solution/Main/app.js
+++ Solution/Main/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var valutationRouter = require('./routes/valutation');
 
 
 var app = express();
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('public'));
 
+app.use('/valutation', valutationRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
