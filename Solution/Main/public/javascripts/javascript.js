6ff24b1e6e4d648c31ff341ed52c367714c89fd0
Alex Agliuzza <alex.agliuzza@edu.unito.it>
2024-01-11 19:23:24 +0100

    Login ejs file

--- Solution/Main/public/javascripts/javascript.js
+++ Solution/Main/public/javascripts/javascript.js
@@ -1,4 +1,49 @@
-function init(){
-    const buttonForm = document.getElementById("submitForm")
-    
+function init() {
+    const form = document.getElementById("form");
+    form.addEventListener('submit', submitForm);
 }
+
+function submitForm(event) {
+    const nickname = document.getElementById('nickname').value;
+    const resultControl = controlInfo(nickname);
+    if (!resultControl) {
+        alert("Nickname or password are wrong. Please check them");
+    } else {
+        const now = new Date();
+        const item = {
+            nickname: nickname,
+            timestamp: now.getTime(), // Ottiene il timestamp corrente in millisecondi
+        };
+        localStorage.setItem('nicknameData', JSON.stringify(item));
+        document.getElementById("form").submit();
+    }
+}
+
+function controlInfo(nickname) {
+    const password = document.getElementById('password').value;
+    const minLength = 6;
+    const maxLength = 15;
+
+    if(password === '' || nickname === '')
+        return false;
+
+    if(nickname.lenght > 15)
+        return false
+    // Check for minimum and maximum length
+    if (password.length < minLength || password.length > maxLength)
+        return false;
+
+    // Use a regular expression to check for at least one number
+    const numberRegex = /\d/;
+    if (!numberRegex.test(password))
+        return false;
+
+    // Use a regular expression to check for at least one special character
+    const specialCharRegex = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/;
+    if (!specialCharRegex.test(password))
+        return false;
+
+    // If all checks pass, return true
+    return true;
+}
+
