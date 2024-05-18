1a7587fb14acc2eeefbeb55cbb92bc9ad0c34e47
Bilguun Batsaikhan <bilguun.batsaikhan@edu.unito.it>
2024-05-18 18:04:16 +0200

    Now player table should look somewhat okay

--- Solution/Main/public/javascripts/popular_players.js
+++ Solution/Main/public/javascripts/popular_players.js
@@ -2,7 +2,7 @@ async function getPopularPlayers() {
     try {
         const responseData = await sendAxiosIndexQuery('/getpopularplayers');
         const players = responseData.popPlayers;
-        //console.log("Popular Players: " + JSON.stringify(players));
+
         for (let i = 0; i < players.length; i++) {
             const player = players[i];
             const imgElement = document.getElementById(`player-${i + 1}`);
@@ -10,6 +10,7 @@ async function getPopularPlayers() {
             if (imgElement) {
                 const linkElement = document.createElement('a');
                 linkElement.href = '../valutation/player?player_id=' + player.playerId;
+                linkElement.classList.add('player-link');  // Add your class here
 
                 imgElement.parentNode.insertBefore(linkElement, imgElement);
                 linkElement.appendChild(imgElement);
@@ -21,4 +22,3 @@ async function getPopularPlayers() {
         console.error("Error fetching data:", error);
     }
 }
-
