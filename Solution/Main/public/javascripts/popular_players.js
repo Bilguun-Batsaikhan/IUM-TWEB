6210db0d8a9148e7be3d1b8b2bdacaaa726366ac
Emanuele Machetti <emanuele.machetti@edu.unito.it>
2024-05-19 18:13:43 +0200

    Fixed CSS issue on homepage for popular players

--- Solution/Main/public/javascripts/popular_players.js
+++ Solution/Main/public/javascripts/popular_players.js
@@ -6,14 +6,15 @@ async function getPopularPlayers() {
         for (let i = 0; i < players.length; i++) {
             const player = players[i];
             const imgElement = document.getElementById(`player-${i + 1}`);
+            const aElement = document.getElementById(`player-link-${i + 1}`);
 
             if (imgElement) {
-                const linkElement = document.createElement('a');
-                linkElement.href = '../valutation/player?player_id=' + player.playerId;
-                linkElement.classList.add('player-link');  // Add your class here
+                //const linkElement = document.createElement('a');
+                aElement.href = '../valutation/player?player_id=' + player.playerId;
+                //linkElement.classList.add('player-link');  // Add your class here
 
-                imgElement.parentNode.insertBefore(linkElement, imgElement);
-                linkElement.appendChild(imgElement);
+                //imgElement.parentNode.insertBefore(linkElement, imgElement);
+                //linkElement.appendChild(imgElement);
 
                 imgElement.src = player.imageUrl;
             }
