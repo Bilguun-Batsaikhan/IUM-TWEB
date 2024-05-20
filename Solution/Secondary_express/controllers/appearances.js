5fff00f086ae2a30af8ec6db12e9288655442e49
Alex Agliuzza <alex.agliuzza@edu.unito.it>
2024-05-20 18:09:19 +0200

    Adding 2 query for appearances

---
+++ Solution/Secondary_express/controllers/appearances.js
@@ -0,0 +1,35 @@
+const Model = require('../models/Appearances');
+
+/* it obtains all Appearances of the competition specified by ID */
+function getAppearancesByPlayer(player_id) {
+    return new Promise((resolve, reject) => {
+        console.log("Querying for player_id:", player_id); // Log per debug
+        Model.find({ player_id: player_id })
+            .then(results => {
+                console.log("Query results:", results); // Log per debug
+                resolve(results);
+            })
+            .catch(error => {
+                console.error("Query error:", error); // Log per debug
+                reject(error);
+            });
+    });
+}
+
+module.exports.getAppearancesByCompetitionId = getAppearancesByCompetitionId;
+
+function getAppearancesByCompetitionId(competition_id) {
+    return new Promise((resolve, reject) => {// Log per debug
+        Model.find({ competition_id: competition_id })
+            .then(results => {
+                console.log("Query results:", results); // Log per debug
+                resolve(results);
+            })
+            .catch(error => {
+                console.error("Query error:", error); // Log per debug
+                reject(error);
+            });
+    });
+}
+
+module.exports.getAppearancesByPlayer = getAppearancesByPlayer;
--- Solution/Secondary_express/routes/index.js
+++ Solution/Secondary_express/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const gamesController = require('../controllers/games');
 const clubsController = require('../controllers/clubs');
-const clubValutationController = require('../controllers/club_valutation');
+const appearancesController = require('../controllers/appearances');
 var bodyParser = require('body-parser');
 router.use( bodyParser.json() );
 
@@ -36,10 +36,35 @@ router.post('/club', async (req, res) => {
   try {
     // Parse the request body
     const clubSearch = req.body.name;
-    console.log(clubSearch + "Aaaaaaaaaaaaa");
     // Wait for the promise from the 'res.json' call to resolve before accessing the 'clubSearch' property
     const clubNamesSearch = await clubsController.getClubNamesForSearchBar(clubSearch);
     res.json({ result: clubNamesSearch });
+  } catch (error) {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(500).json({ error: 'Error retrieving club names for bar search', message: error.message });
+  }
+});
+
+router.post('/getAppearancesByPlayer', async (req, res) => {
+  try {
+    // Parse the request body
+
+   const appearancesPlayer = req.body.player_id;
+
+    // Wait for the promise from the 'res.json' call to resolve before accessing the 'clubSearch' property
+    const appearances = await appearancesController.getAppearancesByPlayer(appearancesPlayer);
+    res.json({ result: appearances });
+  } catch (error) {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(500).json({ error: 'Error retrieving club names for bar search', message: error.message });
+  }
+});
+router.post('/getAppearancesByCompetitions', async (req, res) => {
+  try {
+    const appearancesCompetitions = req.body.competitions_id;
+    // Wait for the promise from the 'res.json' call to resolve before accessing the 'clubSearch' property
+    const appearances = await appearancesController.getAppearancesByCompetitionId(appearancesCompetitions);
+    res.json({ result: appearances });
   } catch (error) {
     res.setHeader('Content-Type', 'application/json');
     res.status(500).json({ error: 'Error retrieving club names for bar search', message: error.message });
