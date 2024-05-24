25cb032bbedbdafa279464dd0b9a12e516fc7b43
Emanuele Machetti <emanuele.machetti@edu.unito.it>
2024-05-24 18:38:37 +0200

    Started implementation of search bar

--- Solution/Main/app.js
+++ Solution/Main/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var valutationRouter = require('./routes/valutation');
+var searchRouter = require('./routes/search');
 
 
 var app = express();
@@ -24,6 +25,7 @@ app.use(express.static('public'));
 app.use('/valutation', valutationRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/search', searchRouter);
 
 
 // catch 404 and forward to error handler
